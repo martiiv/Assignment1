7d7aad83adf228f21ac80253fc0d237a49639a9f
Martin Iversen <martiiv@stud.ntnu.no>
2023-02-13 13:13:38 +0100

    Working on plotting line

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,7 +1,8 @@
 import pandas as pd #We use pandas for csv file management
-import matplotlib as mat #We use matplotlib for div math
 import matplotlib.pyplot as plt #Pyplot for plotting scatterplot
+import numpy as np
 
+# Task 1.1 
 trainData = pd.read_csv("a1_train.csv",encoding="UTF-8")    #Read from csv 
 x = trainData["AttrX"]                                      #Defining x attribute 
 y = trainData["AttrY"]                                      #Defining y attribute
@@ -9,6 +10,14 @@ attrClass = trainData["Class"]
 
 colors = {"A":"red", "B":"blue"}                            #We map the colors to the different classtypes from the csv file 
 
-fig = plt.figure(figsize=(8,8))                             #Defining figure   
+fig, ax = plt.subplots(figsize=(8,8))                             #Defining figure   
 plt.scatter(x, y, c=attrClass.map(colors))                  #Plotting the scatterplot
+
+
+# Task 1.2 
+LDB1 = (15), (20)
+
+ax.plot(LDB1, "-", label= "Linear decision boundary")
+
+
 plt.show()                                              
