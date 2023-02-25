0f0b73c3921f35581bce2615139385552c7a73e3
Martin Iversen <martiiv@stud.ntnu.no>
2023-02-25 13:17:03 +0100

    Finished assignment_1

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,10 +1,6 @@
 import pandas as pd #We use pandas for csv file management
-import matplotlib
+import matplotlib 
 from matplotlib import pyplot as plt #Pyplot for plotting scatterplot
-import numpy as np
-import scipy 
-from sklearn import svm
-
 
 # Task 1.1 
 trainData = pd.read_csv("a1_train.csv",encoding="UTF-8")    #Read from csv 
@@ -19,9 +15,5 @@ fig, ax = plt.subplots(figsize=(8,8))                       #Defining figure and
 plt.xlabel("AttrX")
 plt.ylabel("AttrY")
 plt.scatter(x, y, c=attrClass.map(colors))                  #Plotting the scatterplot
-
-
-# Task 1.2
-# See a1_task1_2_lines.png
 
 plt.show()                                              
